34cfd881dee661af44e848dd9334a05e4eafc614
liyuqi <a964581583@hotmail.com>
2023-01-29 18:27:06 +0800

    update verify shell

--- shifu-demo/step2/verify.sh
+++ shifu-demo/step2/verify.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if $(kubectl rollout status deployment/shifu-crd-controller-manager -n shifu-crd-system -w=false | grep -q "successfully rolled out"); then
   echo "Deployment shifu-crd-controller-manager is ready"
 else
--- shifu-demo/step4/verify.sh
+++ shifu-demo/step4/verify.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if $(kubectl rollout status deployment/shifu-deployer -n shifu-agent -w=false | grep -q "successfully rolled out"); then
   echo "Deployment shifu-deployer  is ready"
   exit 1
