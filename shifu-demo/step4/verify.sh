df010d99602d7a5bb00ce0a533acd69dbcce7efd
liyuqi <a964581583@hotmail.com>
2023-01-30 11:45:27 +0800

    update verify

--- shifu-demo/step4/verify.sh
+++ shifu-demo/step4/verify.sh
@@ -2,6 +2,7 @@
 
 if $(kubectl rollout status deployment/deviceshifu-hikvision-deployment -n deviceshifu -w=false | grep -q "successfully rolled out"); then
   echo "Deployment deviceshifu-hikvision-deployment is ready"
+  exit 0
 else
   echo "Deployment deviceshifu-hikvision-deployment is not ready"
   exit 1
